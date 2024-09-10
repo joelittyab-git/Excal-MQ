dc050277325c52518980542e1c0f85f4f4f6d25c
joelittyab-git <joeittab@gmail.com>
2024-09-10 23:05:42 +0530

    Socket Methods for I/O and protocol

--- net/src/socket/client/mod.rs
+++ net/src/socket/client/mod.rs
@@ -1,11 +1,14 @@
 pub mod error;
 pub mod data;
 
-use std::net::Ipv4Addr;
+use std::{net::Ipv4Addr, time::Duration};
 
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream, time};
 
 use error::ClientSocketError;
+use crate::protocol::interface::MessageTransferProtocol;
+
+use super::data::ProtocolParser as ProtocolParse;
 
 /// A simple socket for wrapping over async standard tcp stream
 /// Simplifies the tco_stream by returning data in an enclosed entity
@@ -111,4 +114,111 @@ impl ClientSocket {
 
           return Ok(());
      }
+     /// Asynchronously receives data from the TCP stream.
+     ///
+     /// # Arguments
+     ///
+     /// * `buf` - A mutable byte buffer to store the received data.
+     ///
+     /// # Returns
+     ///
+     /// This function returns a `Result<usize, ClientSocketError>`, where:
+     /// - `Ok(n)` returns the number of bytes read.
+     /// - `Err(ClientSocketError)` if an error occurs during the read operation.
+     ///
+     /// # Errors
+     ///
+     /// - `ClientSocketError::IoError` if there's a network or stream issue.
+     ///
+     /// # Example
+     ///
+     /// ```rust
+     /// let mut buf = vec![0; 1024];
+     /// let bytes_read = socket.recv(&mut buf).await.unwrap();
+     /// ```
+     pub async fn recv(&mut self, buf: &mut [u8]) -> Result<usize, ClientSocketError> {
+          let n = self.stream.read(buf).await?;
+          Ok(n)
+     }
+
+     /// Closes the TCP connection gracefully.
+     ///
+     /// # Returns
+     ///
+     /// This function returns a `Result<(), ClientSocketError>`, where:
+     /// - `Ok(())` if the connection is successfully closed.
+     /// - `Err(ClientSocketError)` if an error occurs during the closing process.
+     ///
+     /// # Example
+     ///
+     /// ```rust
+     /// socket.close().await.unwrap();
+     /// ```
+     pub async fn close(&mut self) -> Result<(), ClientSocketError> {
+          self.stream.shutdown().await.map_err(|e| ClientSocketError::IoError { source: e })
+     }
+
+     /// Asynchronously attempts to send data with a timeout.
+     ///
+     /// # Arguments
+     /// 
+     /// * `data` - The string data to send.
+     /// * `timeout_duration` - The duration after which the send operation will timeout.
+     ///
+     /// # Returns
+     ///
+     /// `Ok(())` if the data is successfully sent within the timeout.
+     /// `Err(ClientSocketError)` if it times out or encounters an error.
+     pub async fn send_with_timeout(&mut self, data: String, timeout_duration: Duration) -> Result<(), ClientSocketError> {
+          match time::timeout(timeout_duration, self.stream.write(data.as_bytes())).await {
+               Ok(Ok(_)) => Ok(()),
+               Ok(Err(e)) => Err(ClientSocketError::IoError { source: e }),
+               Err(_) => Err(ClientSocketError::TimeoutError{
+                    message:"Request Timeout".to_string()
+               }),
+          }
+     }
+
+     /// Sends a framed message with a length prefix.
+     ///
+     /// # Arguments
+     ///
+     /// * `data` - The message to send.
+     ///
+     /// # Returns
+     ///
+     /// Sends the length of the message followed by the actual data.
+     pub async fn send_frame(&mut self, data: impl ProtocolParse) -> Result<(), ClientSocketError> {
+          // buffer
+          let bytes:Vec<u8> = match data.to_bytes() {
+               Ok(b) => b,
+               Err(e) => return Err(ClientSocketError::ProtocolParseError { source: e }),
+          };
+
+          match self.stream.write(&bytes).await{
+               Ok(e) => return Ok(()),
+               Err(s) => Err(ClientSocketError::IoError { source: s }),
+          }
+     }
+
+     /// Receives a framed message with a length prefix.
+     ///
+     /// # Returns
+     ///
+     /// The message data received after the length prefix.
+     pub async fn recv_frame<T: MessageTransferProtocol + Clone + ProtocolParse>(
+          &mut self,
+          protocol: &mut T
+      ) -> Result<T, ClientSocketError> {
+          let mut bytes: Vec<u8> = Vec::new();
+          
+          // Reading bytes from stream
+          self.recv(&mut bytes).await.map_err(|e| ClientSocketError::from(e))?;
+          
+          // Parsing protocol data
+          match protocol.from_raw(bytes) {
+              Ok(parsed_data) =>Ok(parsed_data) ,
+              Err(e) => Err(ClientSocketError::ProtocolParseError { source: e }),
+          }
+      }
 }
